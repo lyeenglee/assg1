aaf38da9faf694f77119bad9e2fc67f95efb1f80
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2025-04-09 16:58:14 +0800

    initial commit

--- src/constants/string.js
+++ src/constants/string.js
@@ -2,4 +2,5 @@ export const SIGN_IN = "Sign In";
 export const LOGIN = "Login";
 export const LOGOUT = "Logout";
 export const LOGO = "Logo";
-export const POST_LIST = "Post List";
+export const POST_LIST = "Restaurant Review";
+export const VIEW_DETAILS = "View Details";
--- src/screens/PostList.js
+++ src/screens/PostList.js
@@ -3,12 +3,23 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import NavBar from "../components/topNavBar/NavBar";
-import { Fab } from "@mui/material";
+import {
+  Button,
+  Fab,
+  FormControl,
+  FormHelperText,
+  Input,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import PostModal from "../components/modal/PostModal";
 import { useDispatch, useSelector } from "react-redux";
 import { addPost, resetPost } from "../slices/postSlice";
 import { useNavigate } from "react-router-dom";
+import restaurantList from "../mockData/restaurantList.json";
+import { VIEW_DETAILS } from "../constants/string";
 const PostList = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -17,7 +28,14 @@ const PostList = () => {
 
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const handleCloseModal = () => setIsModalOpen(false);
+  let uniqueCategories = Array.from(
+    new Set(restaurantList.map((item) => item.category))
+  );
+  const [category, setCategory] = React.useState(""); // To handle the value of the select
 
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
   const handleOpenModal = () => {
     setIsModalOpen(true);
   };
@@ -40,15 +58,15 @@ const PostList = () => {
 
   const getPostData = React.useCallback(async () => {
     try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const data = await response.json();
-      const updatedData = data.map((item) => {
-        const randomIndex = getRandomIndex();
+      // const response = await fetch(
+      //   "https://jsonplaceholder.typicode.com/posts"
+      // );
+      // const data = await response.json();
+      const updatedData = restaurantList.map((item) => {
+        // const randomIndex = getRandomIndex();
         return {
           ...item,
-          img: `/assets/img/Gym${randomIndex}.jpg`,
+          // img: `/assets/img/Gym${randomIndex}.jpg`,
         };
       });
       dispatch(addPost(updatedData));
@@ -68,11 +86,67 @@ const PostList = () => {
   return (
     <div className="post-list">
       <NavBar />
+      <div
+        style={{
+          marginTop: "85px",
+          display: "flex",
+          margin: 1,
+          flexDirection: "column",
+          width: "80%",
+        }}
+      >
+        <TextField
+          id="outlined-search"
+          label="Search for a restaurant"
+          type="search"
+          style={{ marginTop: "85px", flex: "1" }}
+        />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <FormControl sx={{ width: "100%", marginTop: "15px" }}>
+            <Select
+              value={category}
+              onChange={handleChange}
+              label="Category"
+              displayEmpty
+              sx={{ textAlign: "left", color: "black" }}
+            >
+              <MenuItem value="">
+                <em>All Categories</em>
+              </MenuItem>
+
+              {uniqueCategories?.map((item) => (
+                <MenuItem key={item} value={item}>
+                  {item}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Button
+            sx={{
+              ml: "auto",
+              textTransform: "none",
+              marginLeft: "8px",
+              // marginY: "8px",
+            }}
+            variant="outlined"
+            onClick={() => {}}
+          >
+            {VIEW_DETAILS}
+          </Button>
+        </div>
+      </div>
+
       <ImageList
         sx={{
           backgroundColor: "#ffffff",
           top: "70px",
-          marginTop: "100px",
+          marginTop: "0",
           display: "flex",
           flexWrap: "wrap",
           justifyContent: "center",
@@ -87,16 +161,14 @@ const PostList = () => {
               backgroundColor: "rgba(203, 213, 225, 0.2)",
               boxShadow: "0px 4px 4px rgba(5, 14, 14, 0.25)",
               boxSizing: "border-box",
+              borderRadius: "15px",
             }}
             key={item.id}
-            onClick={() => {
-              navigate(`/postDetail/${item.id}`);
-            }}
+            onClick={() => {}}
           >
             `
             <img
-              src={item.img}
-              alt="Random"
+              src={item.picture}
               loading="lazy"
               style={{
                 width: "280px",
@@ -107,10 +179,47 @@ const PostList = () => {
               }}
             />
             <ImageListItemBar
-              title={item.title}
+              title={item.name}
               position="below"
-              sx={{ width: "280px", marginLeft: "15px", marginRight: "15px" }}
+              sx={{
+                width: "280px",
+                marginLeft: "15px",
+                marginRight: "15px",
+                textAlign: "left",
+              }}
             />
+            <ImageListItemBar
+              title={item.totRating}
+              position="below"
+              sx={{
+                width: "280px",
+                marginLeft: "15px",
+                marginRight: "15px",
+                textAlign: "left",
+              }}
+            />
+            <ImageListItemBar
+              title={item.category}
+              position="below"
+              sx={{
+                width: "280px",
+                marginLeft: "15px",
+                marginRight: "15px",
+                textAlign: "left",
+              }}
+            />
+            <Button
+              sx={{
+                ml: "auto",
+                textTransform: "none",
+                marginRight: "8px",
+                marginY: "8px",
+              }}
+              variant="outlined"
+              onClick={() => navigate(`/postDetail/${item.id}`)}
+            >
+              {VIEW_DETAILS}
+            </Button>
           </ImageListItem>
         ))}
       </ImageList>
